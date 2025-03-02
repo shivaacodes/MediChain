4e7f05f1e01ed02ab203a92479767ff31d869e0e
shivaacodes <shivasajay007@gmail.com>
2025-03-02 14:32:00 +0530

    fixed v4 configuration

--- medichain/frontend/src/app/page.tsx
+++ medichain/frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-white">
-      <h1 className="text-5xl font-extrabold text-black">MediChain</h1>
+      <h1 className="text-8xl font-extrabold text-amber-500">MediChain</h1>
     </main>
   );
 }
